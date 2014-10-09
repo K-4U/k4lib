5fe0e3fc9316c415202162e10a8dda750d2d799f
Koen Beckers <koen@k-4u.nl>
2014-10-09 22:01:14 +0200

    Redid the modid

--- src/k4unl/minecraft/k4lib/lib/config/ModInfo.java
+++ src/k4unl/minecraft/k4lib/lib/config/ModInfo.java
@@ -1,7 +1,7 @@
 package k4unl.minecraft.k4lib.lib.config;
 
 public class ModInfo {
-	public static final String ID = "K4Lib";
+	public static final String ID = "k4lib";
 	public static final String NAME = "K4Lib";
 	public static final String VERSION = "@VERSION@";
 	public static final String LID = ID.toLowerCase();
