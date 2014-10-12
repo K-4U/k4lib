0df3b1fa8519d91eb5ed755cbc88103e439baf37
Koen Beckers <koen@k-4u.nl>
2014-10-12 19:48:52 +0200

    Added option for doubles and comments

--- src/k4unl/minecraft/k4lib/lib/config/ConfigOption.java
+++ src/k4unl/minecraft/k4lib/lib/config/ConfigOption.java
@@ -10,6 +10,7 @@ public class ConfigOption {
     private boolean isInt;
     private boolean isString;
     private boolean isChar;
+    private boolean isDouble;
 
     private boolean val;
     private boolean def;
@@ -23,7 +24,11 @@ public class ConfigOption {
     private char valChar;
     private char defChar;
 
+    private double valDouble;
+    private double defDouble;
+
     private String category = Configuration.CATEGORY_GENERAL;
+    private String comment = "";
 
     public ConfigOption(String _key, boolean _def) {
         key = _key;
@@ -32,6 +37,7 @@ public class ConfigOption {
         isBool = true;
         isInt = false;
         isString = false;
+        isDouble = false;
     }
 
     public ConfigOption(String _key, int _def){
@@ -41,6 +47,7 @@ public class ConfigOption {
         isBool = false;
         isInt = true;
         isString = false;
+        isDouble = false;
     }
 
     public ConfigOption(String _key, String _def){
@@ -50,6 +57,7 @@ public class ConfigOption {
         isBool = false;
         isInt = false;
         isString = true;
+        isDouble = false;
     }
 
     public ConfigOption(String _key, char _def){
@@ -60,12 +68,38 @@ public class ConfigOption {
         isInt = false;
         isString = false;
         isChar = true;
+        isDouble = false;
+    }
+
+    public ConfigOption(String _key, double _def){
+        key = _key;
+        valDouble = _def;
+        defDouble = _def;
+        isBool = false;
+        isInt = false;
+        isString = false;
+        isChar = false;
+        isDouble = true;
     }
 
     public ConfigOption setCategory(String newCat){
         category = newCat;
         return this;
     }
+
+    public ConfigOption setComment(String newComment){
+        comment = newComment;
+        return this;
+    }
+
+    public String getComment(){
+        return comment;
+    }
+
+    public String getCategory(){
+        return category;
+    }
+
 
     public String getKey(){
         return key;
@@ -87,17 +121,38 @@ public class ConfigOption {
         return valChar;
     }
 
+    public double getDouble(){
+        return valDouble;
+    }
+
     public void loadFromConfig(Configuration config){
         if(isBool){
             val = config.get(category, key, def).getBoolean(def);
+            if(!comment.equals("")) {
+                config.get(category, key, def).comment = comment;
+            }
         }else if(isInt){
             valInt = config.get(category, key, defInt).getInt(defInt);
+            if(!comment.equals("")) {
+                config.get(category, key, defInt).comment = comment;
+            }
         }else if(isString){
             valString = config.get(category, key, defString).getString();
+            if(!comment.equals("")) {
+                config.get(category, key, defString).comment = comment;
+            }
         }else if(isChar){
             String t = config.get(category, key, defChar + "").getString();
+            if(!comment.equals("")) {
+                config.get(category, key, defChar).comment = comment;
+            }
             if(t.length() > 0){
                 valChar = t.charAt(0);
+            }
+        }else if(isDouble){
+            valDouble = config.get(category, key, defDouble).getDouble();
+            if(!comment.equals("")) {
+                config.get(category, key, defDouble).comment = comment;
             }
         }
     }
