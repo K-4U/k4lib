a8a9b343b36961ebaa57663ffabb70b3b71c497d
Koen Beckers <koen@k-4u.nl>
2014-11-26 13:14:35 +0100

    No idea why this file was gone

---
+++ src/k4unl/minecraft/k4lib/lib/config/ConfigHandler.java
@@ -0,0 +1,21 @@
+package k4unl.minecraft.k4lib.lib.config;
+
+import net.minecraftforge.common.config.Configuration;
+
+import java.io.File;
+
+public class ConfigHandler {
+
+    protected Configuration config;
+
+    public void init(Config toLoad, File configFile) {
+
+        config = new Configuration(configFile);
+
+        toLoad.loadConfigOptions(config);
+
+        if (config.hasChanged()){
+            config.save();
+        }
+    }
+}
