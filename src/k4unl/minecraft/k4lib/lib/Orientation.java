95ed4566a327c905772f6399a95a922065d1d0a8
Koen Beckers <koen@k-4u.nl>
2015-06-07 16:20:35 +0200

    Added orientation class

---
+++ src/k4unl/minecraft/k4lib/lib/Orientation.java
@@ -0,0 +1,79 @@
+package k4unl.minecraft.k4lib.lib;
+
+/**
+ * @author K-4U
+ */
+public enum Orientation {
+    FRONT(0), LEFT(1), RIGHT(2), BACK(3), TOP(4), BOTTOM(5);
+
+    private int orientation;
+
+    private Orientation(int or){
+        this.orientation = or;
+    }
+
+    public static Orientation calculateOrientation(int side, int meta){
+        if(meta > 0){
+            if(side == meta){
+                return FRONT;
+            }
+            if(meta == 3){
+                if(side == 2)
+                    return BACK;
+                if(side == 4)
+                    return LEFT;
+                if(side == 5)
+                    return RIGHT;
+            }
+            if(meta == 5){
+                if(side == 2)
+                    return RIGHT;
+                if(side == 3)
+                    return LEFT;
+                if(side == 4){
+                    return BACK;
+                }
+
+            }
+            if(meta == 4){
+                if(side == 2)
+                    return LEFT;
+                if(side == 3)
+                    return RIGHT;
+                if(side == 5)
+                    return BACK;
+            }
+            if(meta == 2){
+                if(side == 3)
+                    return BACK;
+                if(side == 4)
+                    return RIGHT;
+                if(side == 5)
+                    return LEFT;
+            }
+
+            if(side == 0)
+                return BOTTOM;
+            if(side == 1)
+                return TOP;
+
+        }else{
+            switch (side){
+                case 0:
+                    return BOTTOM;
+                case 1:
+                    return TOP;
+                case 2:
+                    return RIGHT;
+                case 3:
+                    return FRONT;
+                case 4:
+                    return LEFT;
+                case 5:
+                    return BACK;
+            }
+        }
+        return FRONT;
+    }
+
+}
