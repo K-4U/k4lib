aba57221a02b778b8e9eff71f6c7f75c3a35454f
Koen Beckers <koen@k-4u.nl>
2015-08-14 14:18:43 +0200

    Fixes K-4U/Hydraulicraft#147

--- src/k4unl/minecraft/k4lib/lib/OreDictionaryHelper.java
+++ src/k4unl/minecraft/k4lib/lib/OreDictionaryHelper.java
@@ -5,15 +5,12 @@ import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraftforge.oredict.OreDictionary;
 
-/**
- * Created by K-4U on 22-6-2015.
- */
 public class OreDictionaryHelper {
 
     public static Block registerBlock(Block block, String name){
+        GameRegistry.registerBlock(block, name);
+        OreDictionary.registerOre(name, block);
         if(!OreDictionary.doesOreNameExist(name)) {
-            GameRegistry.registerBlock(block, name);
-            OreDictionary.registerOre(name, block);
             return block;
         }else{
             return Block.getBlockFromItem(OreDictionary.getOres(name).get(0).getItem());
@@ -21,9 +18,9 @@ public class OreDictionaryHelper {
     }
 
     public static Item registerItem(Item item, String name){
+        GameRegistry.registerItem(item, name);
+        OreDictionary.registerOre(name, item);
         if(!OreDictionary.doesOreNameExist(name)) {
-            GameRegistry.registerItem(item, name);
-            OreDictionary.registerOre(name, item);
             return item;
         }else{
             return OreDictionary.getOres(name).get(0).getItem();
