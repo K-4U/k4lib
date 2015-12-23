7257479119311b30e084b3a2696fad29c5313084
Koen Beckers <koen@k-4u.nl>
2015-12-23 19:42:40 +0100

    Moved teleporter from Hydraulicraft into k4lib

---
+++ src/k4unl/minecraft/k4lib/lib/CustomTeleporter.java
@@ -0,0 +1,33 @@
+package k4unl.minecraft.k4lib.lib;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.Teleporter;
+import net.minecraft.world.WorldServer;
+
+/**
+ * @author Koen Beckers (K-4U)
+ */
+public class CustomTeleporter extends Teleporter {
+
+    public CustomTeleporter(WorldServer par1WorldServer) {
+
+        super(par1WorldServer);
+    }
+
+    @Override
+    public boolean makePortal(Entity entity) {
+
+        return true;
+    }
+
+    @Override
+    public void placeInPortal(Entity entity, float rotationYaw) {
+        int newX = MathHelper.floor_double(entity.posX);
+        int newY = MathHelper.floor_double(entity.posY);
+        int newZ = MathHelper.floor_double(entity.posZ);
+        entity.setLocationAndAngles(newX, newY, newZ, rotationYaw, 0.0F);
+    }
+
+
+}
