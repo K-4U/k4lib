29eec82e8c5d8b48b1a9c3ca7bbec980ad869ad4
Koen Beckers <koen@k-4u.nl>
2015-12-28 14:12:50 +0100

    Added experience functions.

--- src/k4unl/minecraft/k4lib/lib/Functions.java
+++ src/k4unl/minecraft/k4lib/lib/Functions.java
@@ -80,4 +80,49 @@ public class Functions {
         return Boolean.parseBoolean(System.getProperty("k4lib.dev", "false"));
     }
 
+    /**
+     * I copied this code from OpenModsLib!
+     * Be warned, minecraft doesn't update experienceTotal properly, so we have
+     * to do this.
+     *
+     * @param player
+     * @return
+     */
+    public static int getPlayerXP(EntityPlayer player) {
+        return (int)(getExperienceForLevel(player.experienceLevel) + (player.experience * player.xpBarCap()));
+    }
+
+    public static void addPlayerXP(EntityPlayer player, int amount) {
+        int experience = getPlayerXP(player) + amount;
+        player.experienceTotal = experience;
+        player.experienceLevel = getLevelForExperience(experience);
+        int expForLevel = getExperienceForLevel(player.experienceLevel);
+        player.experience = (float)(experience - expForLevel) / (float)player.xpBarCap();
+    }
+
+    public static int getExperienceForLevel(int level) {
+        if (level == 0) { return 0; }
+        if (level > 0 && level < 16) {
+            return level * 17;
+        } else if (level > 15 && level < 31) {
+            return (int)(1.5 * Math.pow(level, 2) - 29.5 * level + 360);
+        } else {
+            return (int)(3.5 * Math.pow(level, 2) - 151.5 * level + 2220);
+        }
+    }
+
+    public static int getXpToNextLevel(int level) {
+        int levelXP = getLevelForExperience(level);
+        int nextXP = getExperienceForLevel(level + 1);
+        return nextXP - levelXP;
+    }
+
+    public static int getLevelForExperience(int experience) {
+        int i = 0;
+        while (getExperienceForLevel(i) <= experience) {
+            i++;
+        }
+        return i - 1;
+    }
+
 }
