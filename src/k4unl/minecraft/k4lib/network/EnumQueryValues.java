52bd945c9d24473ce8b3add1dfb75507aff8c372
Koen Beckers <koen@k-4u.nl>
2016-10-03 21:42:42 +0200

    Added more values to the QueryValues. Probably going to change the name later.

--- src/k4unl/minecraft/k4lib/network/EnumQueryValues.java
+++ src/k4unl/minecraft/k4lib/network/EnumQueryValues.java
@@ -4,7 +4,7 @@ package k4unl.minecraft.k4lib.network;
  * @author Koen Beckers (K-4U)
  */
 public enum EnumQueryValues {
-    INVALID, MISFORMED, TIME, PLAYERS, DAYNIGHT, DIMENSIONS, UPTIME, DEATHS, WEATHER, BLOCKINFO, RF, FLUID;
+    INVALID, MISFORMED, TIME, PLAYERS, DAYNIGHT, DIMENSIONS, UPTIME, DEATHS, WEATHER, BLOCKINFO, RF, FLUID, INVENTORY;
 
     public static EnumQueryValues fromString(String str) {
         for(EnumQueryValues v : values()){
