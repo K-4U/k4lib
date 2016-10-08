4eb0526c58d66a0f919f30365abbd57114ff54b8
Koen Beckers <koen@k-4u.nl>
2016-10-08 22:31:45 +0200

    Added TPS to the values.

--- src/k4unl/minecraft/k4lib/network/EnumSIPValues.java
+++ src/k4unl/minecraft/k4lib/network/EnumSIPValues.java
@@ -4,7 +4,7 @@ package k4unl.minecraft.k4lib.network;
  * @author Koen Beckers (K-4U)
  */
 public enum EnumSIPValues {
-    INVALID, MISFORMED, TIME, PLAYERS, DAYNIGHT, DIMENSIONS, UPTIME, DEATHS, WEATHER, BLOCKINFO, FLUID, INVENTORY;
+    INVALID, MISFORMED, TIME, PLAYERS, DAYNIGHT, DIMENSIONS, UPTIME, DEATHS, WEATHER, BLOCKINFO, FLUID, INVENTORY, TPS;
 
     public static EnumSIPValues fromString(String str) {
         for(EnumSIPValues v : values()){
