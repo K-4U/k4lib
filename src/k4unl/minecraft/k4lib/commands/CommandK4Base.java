16df0d7ec871a9367c6794c5842225277b82e875
Koen Beckers <koen@k-4u.nl>
2017-04-05 21:49:41 +0200

    Made variable protected.

--- src/k4unl/minecraft/k4lib/commands/CommandK4Base.java
+++ src/k4unl/minecraft/k4lib/commands/CommandK4Base.java
@@ -10,7 +10,7 @@ import java.util.List;
  * @author Koen Beckers (K-4U)
  */
 public abstract class CommandK4Base extends CommandBase {
-    private List<String> aliases = new ArrayList<String>();
+    protected List<String> aliases = new ArrayList<String>();
 
     public List getCommandAliases()
     {
